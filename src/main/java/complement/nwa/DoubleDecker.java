5cb7c3302dda6d8db8d11843bebcdd9650c983ea
duckly <duckly_lee@163.com>
2017-09-28 17:19:46 +0800

    update

--- src/main/java/complement/nwa/DoubleDecker.java
+++ src/main/java/complement/nwa/DoubleDecker.java
@@ -31,7 +31,7 @@ public class DoubleDecker {
 	
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+        final int prime = 31;
         int result = 1;
         result = prime * result + this.mDownState;
         result = prime * result + this.mUpState;
