69765fc204b8d870716e9da4c307da92e35650de
duckly <duckly_lee@163.com>
2017-11-13 13:09:09 +0800

    update time out

--- run_complement_pldi.sh
+++ run_complement_pldi.sh
@@ -6,21 +6,21 @@ cd target
 column="FILE,NONUSE, OP_STATES,OP_TRANS,OP_ALPHABET_SIZE";
 column="$column,RESULT_STATES, RESULT_TRANS,ALGORITHM,RUNTIME(ms),STATUS";
 
-time=300; # time bound
+time=305; # time bound
 
 echo "$column" > result-complement.csv
 for case in classes/benchmarks/easy/*.ats
 do
 	# NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -complement easy.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement easy.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# optimized NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -lazys -complement easy-opt.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement easy-opt.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# optimized NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -lazys -lazyb -complement easy-opt1.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement easy-opt1.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 done
@@ -30,15 +30,15 @@ done
 for case in classes/benchmarks/normal/*.ats
 do
 	# NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -complement normal.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement normal.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# optimized NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -lazys -complement normal-opt.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement normal-opt.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# optimized NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -lazys -lazyb -complement normal-opt1.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement normal-opt1.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 done
@@ -49,15 +49,15 @@ done
 for case in classes/benchmarks/difficult/*.ats
 do
     # NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -complement diff.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement diff.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# optimized NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -lazys -complement diff-opt.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement diff-opt.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# optimized NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -lazys -lazyb -complement diff-opt1.ba $case -set 3 >> result-complement.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement diff-opt1.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 done
