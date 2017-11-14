328ff8f5947e26b7078fd2414030ea7399b74502
duckly <duckly_lee@163.com>
2017-11-14 17:13:07 +0800

    add new sh

---
+++ run_complement_pldi_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd target
+
+# output columns
+column="FILE,NONUSE, OP_STATES,OP_TRANS,OP_ALPHABET_SIZE";
+column="$column,RESULT_STATES, RESULT_TRANS,ALGORITHM,RUNTIME(ms),STATUS";
+
+time=305; # time bound
+
+echo "$column" > result-complement-all.csv
+for case in semis/*.ats
+do
+	# NCSB
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement easy.ba $case -set 3 >> result-complement-all.csv";
+	echo $command
+	eval $command
+	# optimized NCSB
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement easy-opt.ba $case -set 3 >> result-complement-all.csv";
+	echo $command
+	eval $command
+	# optimized NCSB
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement easy-opt1.ba $case -set 3 >> result-complement-all.csv";
+	echo $command
+	eval $command
+done
