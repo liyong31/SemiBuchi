a2b3e33207c2bba67e0388445df0767173f0ca28
duckly <duckly_lee@163.com>
2018-02-21 14:58:50 +0800

    rename

--- target/run_complement_pldi.sh
+++ target/run_complement_pldi.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-cd target
-
 # output columns
 column="FILE,NONUSE, OP_STATES,OP_TRANS,OP_ALPHABET_SIZE";
 column="$column,RESULT_STATES, RESULT_TRANS,ALGORITHM,RUNTIME(ms),STATUS";
 
 time=305; # time bound
 
-echo "$column" > result-complement-all-eo.csv
+echo "$column" > result-complement.csv
 for case in semis/*.ats
 do
 	# NCSB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement easy.ba $case -set 3 >> result-complement-all-eo.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement easy.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# NCSB + Antichain
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement easye.ba $case -oe -set 3 >> result-complement-all-eo.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -complement easye.ba $case -oe -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# NCSB + LazyS
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement easy-opt.ba $case -set 3 >> result-complement-all-eo.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement easy-opt.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
     # NCSB + LazyS + Antichain
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement easy-opte.ba $case -oe -set 3 >> result-complement-all-eo.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -complement easy-opte.ba $case -oe -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# NCSB + LazyS + LazyB
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement easy-opt1.ba $case -set 3 >> result-complement-all-eo.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement easy-opt1.ba $case -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 	# NCSB + LazyS + LazyB + Antichain
-	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement easy-opte1.ba $case -oe -set 3 >> result-complement-all-eo.csv";
+	command="timeout $time java -jar SemiBuchi-0.0.1.jar -to 300 -lazys -lazyb -complement easy-opte1.ba $case -oe -set 3 >> result-complement.csv";
 	echo $command
 	eval $command
 done
